2f4fdc7189e8d34c1805eb1b81c6468dbf533953
htduong-181 <thuyduongg1801@gmail.com>
2026-09-24 12:55:54 +0700

    Initial commit - source code

---
+++ QLMuaHangVaNCC_UNETI06_TI17A1HN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QLMuaHangVaNCC_UNETI06_TI17A1HN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
