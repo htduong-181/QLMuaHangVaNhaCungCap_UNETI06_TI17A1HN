b2e79656a5aa172f14c0eb64fb0ff513dd81e3c4
duongbui69 <duong692005@gmail.com>
2026-09-25 08:30:56 +0700

    Update program

--- QLMuaHangVaNCC_UNETI06_TI17A1HN/Program.cs
+++ QLMuaHangVaNCC_UNETI06_TI17A1HN/Program.cs
@@ -1,24 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using QLMuaHangVaNCC_UNETI06_TI17A1HN.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+builder.Services.AddDbContext<DbConnection>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
+
 app.MapStaticAssets();
+
 
 app.MapControllerRoute(
     name: "default",
